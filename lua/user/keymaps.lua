e464ee92d5294d389c77ffbf5082ab2dc8a0dddc
Will Harris <will.t.harris@protonmail.com>
2022-09-20 20:39:11 -0700

    install trouble

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -78,3 +78,8 @@ keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
 keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
 keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
 keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
+
+-- Trouble
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+  {silent = true, noremap = true}
+)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -68,6 +68,8 @@ return packer.startup(function(use)
       }
     end
   }
+  use {"folke/lsp-colors.nvim", commit = "4e6da1984d23da88a947805866580c48fc3cc8d7"}
+
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" }
   use { "lunarvim/darkplus.nvim", commit = "2584cdeefc078351a79073322eb7f14d7fbb1835" }
